dd37286cdebfe6a7fce91d739bf990d4c89ba3dd
VirendraRaut <virendraraut1306@gmail.com>
2024-10-02 14:57:51 +0530

    video play status

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -36,6 +36,10 @@ const Hero = ({
               className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
             ></li>
           </ul>
+          <div className="hero-play">
+            <img onClick={()=>setPlayStatus(!PlayStatus)} src={PlayStatus ? pause_icon : play_icon} alt="" />
+            <p>See the video</p>
+          </div>
         </div>
       </div>
     </div>
