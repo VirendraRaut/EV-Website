2db3c79e4410805680bd0cba793da45fa5ca66a3
VirendraRaut <virendraraut1306@gmail.com>
2024-10-02 12:53:30 +0530

    importing imgaes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,16 @@
-import './Hero.css'
+import "./Hero.css";
+import arrow_btn from "../../assets/arrow_btn.png";
+import pause_icon from "../../assets/pause_icon.png";
+import play_icon from "../../assets/play_icon.png";
 
-const Hero = () => {
-  return (
-    <div className='hero'>
-      
-    </div>
-  )
-}
+const Hero = ({
+  heroData,
+  setPlayStatus,
+  heroCount,
+  setHeroCount,
+  PlayStatus,
+}) => {
+  return <div className="hero"></div>;
+};
 
-export default Hero
+export default Hero;
